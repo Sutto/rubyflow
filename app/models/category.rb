c9b4a75e265ed5c07d3ec947a0663308a6f60ea7
Josh Nichols <josh@technicalpickles.com>
2008-08-06 15:38:21 -0400

    Added unit test for category.

--- app/models/category.rb
+++ app/models/category.rb
@@ -3,6 +3,7 @@ class Category < ActiveRecord::Base
   
   validates_presence_of     :title, :parent_id
   validates_length_of       :title, :within => 4..255
+  
   validates_uniqueness_of   :name, :if => :name?
   validates_format_of       :name, :with => /^[\w\-\_]+$/, :if => :name?, :message => 'is invalid (alphanumerics, hyphens and underscores only)'
   validates_length_of       :name, :within => 4..255, :if => :name?
--- test/unit/category_test.rb
+++ test/unit/category_test.rb
@@ -1,8 +1,32 @@
 require File.dirname(__FILE__) + '/../test_helper'
 
 class CategoryTest < ActiveSupport::TestCase
-  # Replace this with your real tests.
-  def test_truth
-    assert true
+  
+  should_require_attributes :title, :parent_id
+  should_ensure_length_in_range :title, (4..255)
+  should_require_unique_attributes :name
+  should_ensure_length_in_range :name, (4..255)
+  # TODO test validates_format_of values
+
+  context 'Category with a short name' do
+    setup do
+      @category = Category.create(:name => 'yuk')
+    end
+    
+    should 'use id for #to_param' do
+      assert_equal @category.id, @category.to_param
+    end
   end
+  
+  context 'Category with long name' do
+    setup do
+      @category = Category.create(:name => 'ever-so-slightly-longer')
+    end
+    
+    should 'use name for #to_param' do
+      assert_equal @category.name, @category.to_param
+    end
+  end
+  
+  # TODO contexts for Category with short/long title to test publicly_facing_name
 end
