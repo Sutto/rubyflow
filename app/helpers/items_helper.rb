97015bb3c12370f3646e9f68f7b3ecb753ca2fc1
Dennis Theisen <dennis@jadedpixel.com>
2008-08-08 03:01:11 -0400

    Minor usability tweaks:   * automatically insert a link for URLs in item body   * minor redesign for starring   * 'Update item' instead of 'Post item' when editing Also:   * Javascript is deactivated per default   * Editing an item is only possible for 30 minutes after last update   * added a test and production database to database.yml

--- app/helpers/items_helper.rb
+++ app/helpers/items_helper.rb
@@ -11,6 +11,21 @@ module ItemsHelper
   def star_link(item)
     starred = item.is_starred_by_user(current_user)
     path    = starred ? item_remove_star_path(item) : item_add_star_path(item)
-    return content_tag(:span, link_to("#{starred ? 'unstar' : 'star'} this post", path, :class => item.starred_class(current_user)), :class => "star")
+    if starred
+      return " &ndash; " + content_tag(:span, link_to("unstar this post", item_remove_star_path(item), :class => item.starred_class(current_user)), :class => "star")
+    end
   end
+  
+  # Convert URLs in text automatically to links
+  def auto_link(text)
+    text.gsub(/ (http:\/\/[^ !,\?]+)/, ' <a href="\1">\1</a>')
+  end
+  
+  # Shows the time left to edit the current item, or nil if it's always allowed
+  def edit_time_left(item)
+    diff = Time.now - item.updated_at
+    expire_rate = APP_CONFIG[:edit_expiration_in_minutes].to_i
+    return expire_rate > 0 ? expire_rate - (diff.to_i / 60) : nil
+  end
+  
 end
